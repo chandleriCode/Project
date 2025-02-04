7a07ae9b302d883a12140cf00516c01b74d09862
chandleriCode <chandler@icodeschool.com>
2025-02-03 17:04:28 -0700

    fix sound

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 const fullstoppuncuation = new Audio("./sound/full-stop-punctuation.mp3");
-window.addEventListener("mousemove",()=>{
-    fullstoppuncuation.play();
+document.querySelector('.death').addEventListener('click', () => {
+    document.getElementById('audio').play();
+});
+
+
+
+document.getElementById("play").addEventListener("click", function ()  {
+    let audio = document.getElementById("audio");
+    let jumpscare = document.getElementById("jumpscare");
+
+    // Play the audio & show the jumpscare
+    audio.play();
+    jumpscare.style.display = "flex";
+
+    // Hide jumpscare after 3 seconds (adjust timing if needed)
+    setTimeout (()=> {
+        jumpscare.style.display = "none";
+    },4700);
 });
