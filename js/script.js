eb4d1b5d43e2ba65769bf55f98e3a9e64e40dbda
chandleriCode <chandler@icodeschool.com>
2025-02-17 16:21:31 -0700

    Changed position

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ document.querySelector('.death').addEventListener('click', () => {
     document.getElementById('audio').play();
 });
 
-
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+const footer = document.querySelector('footer');
 
 document.getElementById("play").addEventListener("click", function ()  {
     let audio = document.getElementById("audio");
@@ -13,8 +15,22 @@ document.getElementById("play").addEventListener("click", function ()  {
     audio.play();
     jumpscare.style.display = "flex";
 
+    body.style.backgroundColor = 'red'; // Change background color to red
+    body.style.color = 'black'; // Change color to black
+    header.style.backgroundColor = 'black'; // Change background color to black
+    header.style.color = 'red'; // Change color to red
+    footer.style.backgroundColor = 'black'; // Change background color to black
+    footer.style.color = 'red'; // Change color to red
+
     // Hide jumpscare after 3 seconds (adjust timing if needed)
     setTimeout (()=> {
         jumpscare.style.display = "none";
-    },4700);
+        body.style.backgroundColor = '';
+        body.style.color = '';
+        header.style.backgroundColor = '';
+        header.style.color = '';
+        footer.style.backgroundColor = '';
+        footer.style.color = '';
+        audio.pause(); // Pause the audio
+    },2300);
 });
