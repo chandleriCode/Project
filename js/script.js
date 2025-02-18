b46b901f24d7a4a77ddb9dcc4998b1691bc5b85e
CombineSoldier14 <101304773+CombineSoldier14@users.noreply.github.com>
2025-02-17 16:12:28 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,22 +15,13 @@ document.getElementById("play").addEventListener("click", function ()  {
     audio.play();
     jumpscare.style.display = "flex";
 
-    body.style.backgroundColor = 'red'; // Change background color to red
-    body.style.color = 'black'; // Change color to black
-    header.style.backgroundColor = 'black'; // Change background color to black
-    header.style.color = 'red'; // Change color to red
-    footer.style.backgroundColor = 'black'; // Change background color to black
-    footer.style.color = 'red'; // Change color to red
+    body.style.backgroundColor = header.style.color = footer.style.color = 'red'; // Change colors to red
+    body.style.color = header.style.backgroundColor = footer.style.backgroundColor = 'black'; // Change colors to black
 
     // Hide jumpscare after 3 seconds (adjust timing if needed)
     setTimeout (()=> {
         jumpscare.style.display = "none";
-        body.style.backgroundColor = '';
-        body.style.color = '';
-        header.style.backgroundColor = '';
-        header.style.color = '';
-        footer.style.backgroundColor = '';
-        footer.style.color = '';
+        body.style.backgroundColor = body.style.color = header.style.backgroundColor = header.style.color = footer.style.backgroundColor = footer.style.color ='';
         audio.pause(); // Pause the audio
     },2300);
 });
